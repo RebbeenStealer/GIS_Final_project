08ee02dde44236965813076b2ac8ddf4da7e0442
RBS <153577632+RebbeenStealer@users.noreply.github.com>
2024-12-18 13:52:15 +0900

    유저의 현재위치와 맵 연동
    geolocation을 활용하여 유저의 현재 위치를 받아오되, 좀 더 정확한 측정을 위해
    {
              enableHighAccuracy: true,
              timeout: 10000, // 10 seconds
              maximumAge: 0,
    }
    속성을 추가하였습니다.

--- react_map_box/src/components/Map.jsx
+++ react_map_box/src/components/Map.jsx
@@ -1,15 +1,18 @@
+import { useState, useEffect } from "react";
 import { Map } from "react-kakao-maps-sdk"
 import "../styles/mapStyle.css";
+import useSingleLocation from "../hooks/location/useSingleLocation";
 
 const kakaoMap = () => {
+
+    const { location, error } = useSingleLocation();
+    const defaultCenter = { lat: 37.5665, lng: 126.9780 };
+
 
     return (
         <Map
             id = "map"
-            center = {{
-                lat: 33.450701,
-                lng: 126.570667,
-            }}
+            center = {location || defaultCenter}
             style = {{
                 width : "100%",
                 height : "100vh"
