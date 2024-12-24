a51e97c9ac32b7387a17bc940647c19fb441d5e4
minjae <whalswo412@gmail.com>
2024-12-24 14:24:47 +0900

    Auto stash before rebase of "main" onto "origin/main"
    asdads

--- door_scanner_api_server_final/src/api/seoul/index.ts
+++ door_scanner_api_server_final/src/api/seoul/index.ts
@@ -7,17 +7,8 @@ const router = express.Router();
 // 도시데이터 ex) /data/citydata?query=가산디지털단지역
 router.get('/citydata', ctrl.getCityData)
 
-<<<<<<< Updated upstream
 // 인구데이터 ex) /data/citydata_ppltn?query=가산디지털잔지역
 router.get('/citydata_ppltn', ctrl.getCityData_ppltn)
 
-=======
-// 인구데이터 ex) /data/citydata_ppltn?query=가산디지털단지역
-router.get('/citydata_ppltn', ctrl.getCityData_ppltn)
-
-// 날씨데이터 ex) /data/weather?query=가산디지털단지역
-router.get('/weather', ctrl.getWeather)
-
->>>>>>> Stashed changes
 
 export default router;
--- react_map_box/src/App.jsx
+++ react_map_box/src/App.jsx
@@ -1,11 +1,6 @@
 import React from 'react';
-<<<<<<< HEAD
 // import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Map from './components/Map';
-=======
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import KakaoMap from './components/KakaoMap';
->>>>>>> 914e677 (hyerin수정작업)
 import './styles/global.css';
 import SignupForm from './components/SignupForm'
 
